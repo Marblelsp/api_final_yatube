d48ca1758c1742fdf44e1de94d3d750466a4cac0
Михаил <kashunin.mihail@yandex.ru>
2021-03-11 00:38:10 +0300

    api_2

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,8 @@ router.register('follow', FollowViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
